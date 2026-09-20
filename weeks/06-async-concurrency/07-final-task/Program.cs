62fef3be71f0b37c05f2d2e745f61798af65e027
s8ession <s8ession@gmail.com>
2026-09-21 01:11:45 +0300

    Эксперимент A

--- weeks/06-async-concurrency/07-final-task/Program.cs
+++ weeks/06-async-concurrency/07-final-task/Program.cs
@@ -1,7 +1,7 @@
 ﻿List<DocumentFile> documentList = new List<DocumentFile>();
 
 DocumentFile documentA = new("document-a.pdf", 100, 200, 300);
-DocumentFile documentB = new("document-b.pdf", 200, 300, 400, true);
+DocumentFile documentB = new("document-b.pdf", 200, 300, 400);
 DocumentFile documentC = new("document-c.pdf", 300, 400, 500);
 DocumentFile documentD = new("document-d.pdf", 400, 500, 600);
 DocumentFile documentE = new("document-e.pdf", 500, 600, 700);
@@ -25,3 +25,8 @@ using CancellationTokenSource cts = new CancellationTokenSource();
 Task experimentA = presenter.ShowAsync(documents, 3, CancellationToken.None);
 
 await experimentA;
+
+Console.WriteLine($"Status: {experimentA.Status}");
+Console.WriteLine($"IsCompleted: {experimentA.IsCompleted}");
+Console.WriteLine($"IsCanceled: {experimentA.IsCanceled}");
+Console.WriteLine($"IsFaulted: {experimentA.IsFaulted}");
