217a5e741b3d97118e0202ab6da7a76fb34542f1
s8ession <s8ession@gmail.com>
2026-09-20 21:53:01 +0300

    Experiment C: Ручная отмена задач загрузки

--- weeks/06-async-concurrency/06-concurrency-control/Program.cs
+++ weeks/06-async-concurrency/06-concurrency-control/Program.cs
@@ -1,9 +1,9 @@
 ﻿// 1. Создаем изменяемый список (List)
 List<ImageFile> imageList = new List<ImageFile>();  
 
-for (int i = 0; i < 100; i++) 
+for (int i = 0; i < 10; i++) 
 {
-    imageList.Add(new ImageFile("image-" + i + ".jpg", 1000));
+    imageList.Add(new ImageFile("image-" + i + ".jpg", 2000 + i * 100));
 }
 
 // 2. Присваиваем его коллекции только для чтения
@@ -15,7 +15,18 @@ ConcurrencyLoggingPresenter presenter = new(scenario);
 UnsafeLoggingPresenter unsafePresenter = new(scenario);
 
 using CancellationTokenSource cts = new CancellationTokenSource();
-//await presenter.ShowAsync(images, 3, cts.Token);
+Task task = presenter.ShowAsync(images, 2, cts.Token);
 
-await unsafePresenter.ShowAsync(images, cts.Token);
-await presenter.ShowUnboundedAsync(images, cts.Token);
+await Task.Delay(500);
+cts.Cancel();
+
+try { await task; }
+catch (OperationCanceledException)
+{
+    Console.WriteLine("Operation cancelled.");
+}
+
+Console.WriteLine($"Task status: {task.Status}");
+Console.WriteLine($"Task is completed: {task.IsCompleted}");
+Console.WriteLine($"Task is canceled: {task.IsCanceled}");
+Console.WriteLine($"Task is faulted: {task.IsFaulted}");
