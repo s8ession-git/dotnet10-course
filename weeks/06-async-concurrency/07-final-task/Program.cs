e7c1c359208a3027aa7988805cd2eb5201138af9
s8ession <s8ession@gmail.com>
2026-09-21 01:16:04 +0300

    Эксперимент B

--- weeks/06-async-concurrency/07-final-task/Program.cs
+++ weeks/06-async-concurrency/07-final-task/Program.cs
@@ -1,14 +1,19 @@
-﻿List<DocumentFile> documentList = new List<DocumentFile>();
+﻿List<DocumentFile> documentListA = new List<DocumentFile>();
+List<DocumentFile> documentListB = new List<DocumentFile>();
 
-DocumentFile documentA = new("document-a.pdf", 100, 200, 300);
-DocumentFile documentB = new("document-b.pdf", 200, 300, 400);
-DocumentFile documentC = new("document-c.pdf", 300, 400, 500);
-DocumentFile documentD = new("document-d.pdf", 400, 500, 600);
-DocumentFile documentE = new("document-e.pdf", 500, 600, 700);
+DocumentFile document1 = new("document-a.pdf", 100, 200, 300);
+DocumentFile document2 = new("document-b.pdf", 200, 300, 400);
+DocumentFile document2Fail = new("document-b.pdf", 200, 300, 400, true);
+DocumentFile document3 = new("document-c.pdf", 300, 400, 500);
+DocumentFile document4 = new("document-d.pdf", 400, 500, 600);
+DocumentFile document4Fail = new("document-d.pdf", 400, 500, 600, true);
+DocumentFile document5 = new("document-e.pdf", 500, 600, 700);
 
-documentList.AddRange(documentA, documentB, documentC, documentD, documentE);
+documentListA.AddRange(document1, document2, document3, document4, document5);
+documentListB.AddRange(document1, document2Fail, document3, document4Fail, document5);
 
-IReadOnlyCollection<DocumentFile> documents = documentList;
+IReadOnlyCollection<DocumentFile> documentsA = documentListA;
+IReadOnlyCollection<DocumentFile> documentsB = documentListB;
 
 
 DummyDocumentValidator documentValidator = new();
@@ -20,11 +25,18 @@ DummyParseDocumentService parseDocumentService = new(documentValidator, document
 MainScenarioPresenter presenter = new(new MainScenario(parseDocumentService));
 
 using CancellationTokenSource cts = new CancellationTokenSource();
-//Task task = presenter.ShowAsync(documents, 2, cts.Token);
 
-Task experimentA = presenter.ShowAsync(documents, 3, CancellationToken.None);
+Task experimentA = presenter.ShowAsync(documentsA, 3, CancellationToken.None);
 
 await experimentA;
+
+Console.WriteLine($"Status: {experimentA.Status}");
+Console.WriteLine($"IsCompleted: {experimentA.IsCompleted}");
+Console.WriteLine($"IsCanceled: {experimentA.IsCanceled}");
+Console.WriteLine($"IsFaulted: {experimentA.IsFaulted}");
+
+Task experimentB = presenter.ShowAsync(documentsB, 3, CancellationToken.None);
+await experimentB;
 
 Console.WriteLine($"Status: {experimentA.Status}");
 Console.WriteLine($"IsCompleted: {experimentA.IsCompleted}");
