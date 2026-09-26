ed4dc852124c1644520b9cd6f20b556d958e4a58
s8ession <s8ession@gmail.com>
2026-09-26 04:31:45 +0300

    Аггрегирующие функции и работа с ними

--- weeks/07-postgresql-basics/05-aggregates-group-by-having/Aggregates.sql
+++ weeks/07-postgresql-basics/05-aggregates-group-by-having/Aggregates.sql
@@ -4,7 +4,8 @@ select count(*) booking_count from bookings;
 -- 2
 select sum(bi.ticket_count) total_tickets, min(bi.unit_price) min_unit_price, max(bi.unit_price) max_unit_price, avg(bi.unit_price) avg_unit_price 
 from booking_items bi 
-group by booking_id;
+join bookings b on b.id = bi.booking_id 
+group by b.id;
 
 -- 3
 select status, count(status) booking_count
@@ -27,15 +28,15 @@ group by bi.booking_id
 order by 1;
 
 -- 6
-select b.customer_id, count(b.customer_id) booking_count
+select c.id, count(c.id) booking_count
 from bookings b 
 right join customers c 
 on b.customer_id = c.id
-group by 1
+group by c.id
 order by 1;
 
 -- 7
-select bi.event_id, sum(bi.ticket_count), sum(bi.unit_price) 
+select bi.event_id, sum(bi.ticket_count) total_tickets, sum(bi.ticket_count * bi.unit_price) confirmed_total
 from events e 
 join booking_items bi on e.id = bi.event_id
 group by bi.event_id;
@@ -50,7 +51,7 @@ having count(b.status) >= 2
 order by 1;
 
 -- 9
-select bi.event_id, sum(ticket_count) total_tickets, count(b.status) confirmed_total 
+select bi.event_id, sum(bi.ticket_count) total_tickets, count(bi.ticket_count * bi.unit_price) confirmed_total 
 from booking_items bi  
 join bookings b on b.id = bi.booking_id 
 join events e on e.id = bi.event_id
@@ -59,9 +60,13 @@ group by bi.event_id
 having sum(ticket_count) >= 5;
 
 -- 10
-select c.id, count(distinct(b.status)) booking_count, sum(coalesce(bi.ticket_count, 0)) total_tickets, sum(coalesce(bi.unit_price, 0))
+select c.id, count(distinct(b.id)) booking_count, count(coalesce(bi.ticket_count, 0)) total_tickets, sum(coalesce(bi.unit_price, 0))
 from bookings b 
-left join booking_items bi on b.id = bi.booking_id
+join booking_items bi on b.id = bi.booking_id
 right join customers c on b.customer_id = c.id
 group by c.id
 order by 1;
+
+select * from bookings;
+select * from booking_items bi;
+select * from customers;
