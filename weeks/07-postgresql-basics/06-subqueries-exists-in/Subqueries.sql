32bdfc69f8ad4051b223799f0e8e9d2024f578a6
s8ession <s8ession@gmail.com>
2026-09-26 14:05:12 +0300

    Подзапросы/вложенные запросы

---
+++ weeks/07-postgresql-basics/06-subqueries-exists-in/Subqueries.sql
@@ -0,0 +1,59 @@
+-- 1
+select id, title, price 
+from events 
+where price > (select avg(price) from events)
+order by price desc, id asc;
+
+-- 2
+select c.id customer_id, c.display_name, 
+(
+	select count(*)
+	from bookings b
+	where b.customer_id = c.id
+) as booking_count
+from customers c
+order by c.id;
+
+-- 3
+select c.id customer_id, c.display_name
+from customers c 
+where c.id in (select b.customer_id from bookings b where b.status='confirmed')
+order by 1;
+
+-- 4
+select c.id customer_id, c.display_name
+from customers c
+where exists (select 1 from bookings b where b.status ='confirmed' and b.customer_id = c.id)
+order by 1;
+
+-- 5
+select c.id customer_id, c.display_name
+from customers c 
+where not exists (select 1 from bookings b where b.status = 'confirmed' and b.customer_id = c.id)
+order by 1;
+
+-- 6
+select b.id booking_id, b.customer_id, b.status   
+from bookings b
+where not exists (select 1 from booking_items bi where bi.booking_id = b.id)
+order by  1;
+
+-- 7
+select 
+	total.booking_id,
+	total.booking_total
+from
+(
+	select bi.booking_id, sum(bi.ticket_count * bi.unit_price) booking_total 
+	from booking_items bi
+	group by bi.booking_id
+) as total
+where total.booking_total >= 1000
+order by 2 desc, 1 asc;
+
+-- 8 
+select c.id customer_id, c.display_name
+from customers c
+where 
+exists (select 1 from bookings b where b.customer_id = c.id and b.status = 'confirmed') and 
+not exists (select 1 from booking_items bi join bookings b on b.id = bi.booking_id where b.customer_id = c.id);
